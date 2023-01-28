edfb30a335c8c40a0117bf5f397237cb0ec74d6b
fu-jo <56369799+fu-jo@users.noreply.github.com>
2023-01-28 16:27:25 -0500

    installed firebase on frontend and backend Initialized firestore on backend

--- mindalism/server/server.py
+++ mindalism/server/server.py
@@ -1,18 +1,22 @@
 # Import flask and datetime module for showing date and time
 from flask import Flask
-from SpeechToText import speechtotext
-import datetime
-  
-x = datetime.datetime.now()
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+# from SpeechToText import speechtotext
+
+cred = credentials.Certificate("firebase_admin_cred.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
   
 # Initializing flask app
 app = Flask(__name__)
   
   
 # Route for seeing a data
-@app.route('/toTextJournal', methods=['GET'])
-def toTextJournal():
-    return speechtotext.main()
+# @app.route('/toTextJournal', methods=['GET'])
+# def toTextJournal():
+#     return speechtotext.main()
       
 # Running app
 if __name__ == '__main__':
