bd64106fb29c65016d8797e1a2511d388ffc29fe
fu-jo <56369799+fu-jo@users.noreply.github.com>
2023-01-28 14:45:27 -0500

    Minor edits to Calender, Journal added custom date css to DatePicker.css

--- mindalism/client/src/components/Journal.js
+++ mindalism/client/src/components/Journal.js
@@ -5,9 +5,18 @@ import { Typography, Grid, Card, CardMedia, CardContent, CardActionArea, Dialog,
 const Journal = () => {
     return(
         <div>
-            <Typography>
-                Journal!
-            </Typography>
+            <Grid container spacing={4} sx={{p: '2.5%'}}>
+                <Grid item xs={4} sx={{p:'2.5%'}}>
+                    <Typography variant="h2">
+                        Your Journal
+                    </Typography>
+                </Grid>
+                <Grid item xs={4} sx={{p:'2.5%'}}>
+                    <Typography variant="h2">
+                        Journal Entry
+                    </Typography>
+                </Grid>
+            </Grid>
         </div>
     );
 };
