f84bb14deeb42eb633b0a1b7c31a2bd3f477195d
fu-jo <56369799+fu-jo@users.noreply.github.com>
2023-01-28 11:52:00 -0500

    Added packages Removed old content, added new pages

--- mindalism/client/src/App.js
+++ mindalism/client/src/App.js
@@ -3,41 +3,21 @@ import React, { useState, useEffect } from "react";
 import LandingPage from "./components/LandingPage";
 import ErrorPage from "./components/ErrorPage";
 import Navbar from "./components/Navbar";
+import Calendar from "./components/Calendar";
+import Analytics from "./components/Analytics";
+import Journal from "./components/Journal";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
   
 function App() {
-    // usestate for setting a javascript
-    // object for storing and using data
-    const [data, setdata] = useState({
-        name: "",
-        age: 0,
-        date: "",
-        programming: "",
-    });
-  
-    // Using useEffect for single rendering
-    useEffect(() => {
-        // Using fetch to fetch the api from 
-        // flask server it will be redirected to proxy
-        fetch("/data").then((res) =>
-            res.json().then((data) => {
-                // Setting a data from api
-                setdata({
-                    name: data.Name,
-                    age: data.Age,
-                    date: data.Date,
-                    programming: data.programming,
-                });
-            })
-        );
-    }, []);
-  
     return (
     <BrowserRouter>
         <Navbar />
         <Routes>
             <Route path="/" exact element={<LandingPage />} />
+            <Route path="/calendar" exact element={<Calendar/>} />
+            <Route path="/journal" exact element={<Journal/>} />
+            <Route path="/analytics" exact element={<Analytics/>} />
             <Route path="*" element={<ErrorPage />} />
         </Routes>
     </BrowserRouter>
